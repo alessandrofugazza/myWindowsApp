3f84c55ffd696e558098dc720d05c3ca37a84038
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2026-06-10 13:11:33 +0200

    add taskbar icon

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QCoreApplication::setOrganizationName("shell");
     QCoreApplication::setApplicationName("MyWindowsApp");
+    a.setWindowIcon(QIcon(":/icons/rsc/img/apple-touch-icon.png"));
     MainWindow w;
     w.setWindowTitle("My Windows App");
     w.showMaximized();
